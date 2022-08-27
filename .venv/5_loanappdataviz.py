8544a00ed1c080807f885aeae90d719bda52745b
calappen <chrislappen@gmail.com>
2022-08-27 13:57:01 -0700

    Completed graph showing application approvals based on property area. Added screenshot to folder. Modified section 5.1 and updated screenshot

--- .venv/5_loanappdataviz.py
+++ .venv/5_loanappdataviz.py
@@ -13,20 +13,37 @@ loan_df=spark.read.format("jdbc").options(driver="com.mysql.cj.jdbc.Driver",\
                                      url="jdbc:mysql://localhost:3306/creditcard_capstone",\
                                      dbtable="creditcard_capstone.cdw_sapp_loan_application").load().toPandas()
 
+
 married_income = loan_df[['Application_Status','Gender','Income']]
 married_income = married_income[(married_income['Application_Status']=='Y')].drop(['Application_Status'], axis=1)
 married_income = married_income.groupby('Gender')['Income'].value_counts().unstack()
 married_income = married_income.reindex(columns=['low','medium','high'])
 
-married_income.plot(kind='bar', figsize=(10, 5), ylabel='Application Approvals',
+bar_width = 0.9
+num_of_bars = len(married_income.columns)
+married_income.plot(kind='bar', figsize=(10, 5), ylabel='Application Approvals', width=bar_width,
                     title='Application Approvals of Married Applicants Based on Gender and Income Ranges')
 
-for i in range(len(married_income)):
-    plt.text(i-0.19,married_income['low'][i]+1.5,married_income['low'][i])
-    plt.text(i-0.02,married_income['medium'][i]+1.5,married_income['medium'][i])
-    plt.text(i+0.15,married_income['high'][i]+1.5,married_income['high'][i])
+spacing = -bar_width/num_of_bars
+for income, counts in married_income.items():
+    for index, count in enumerate(counts):
+        plt.text(index+spacing, count+2, count, ha='center')
+    spacing += bar_width/num_of_bars
     
 plt.xticks(rotation=0)
+plt.tight_layout()
+plt.show()
+
+
+bar_width=0.5
+prop_area = loan_df[['Application_Status','Property_Area']]
+prop_area = prop_area.groupby('Application_Status')['Property_Area'].value_counts().unstack()
+prop_area.plot(kind='barh', figsize=(10,5), width=bar_width,
+               stacked=True, xlabel='Application Status',
+               title='Application Approvals Based on Property Area')
+
+tick_labels = ['Denied','Approved']
+plt.yticks(ticks=range(len(tick_labels)), labels=tick_labels, rotation=0)
 plt.tight_layout()
 plt.show()
 
