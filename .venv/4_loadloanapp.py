3ce4156eca0670a094aa3dbde12036af0c8d1f7b
calappen <chrislappen@gmail.com>
2022-08-27 07:19:44 -0700

    The API endpoint's status code is printed to console.

--- .venv/4_loadloanapp.py
+++ .venv/4_loadloanapp.py
@@ -3,3 +3,5 @@ import requests
 api_url = 'https://raw.githubusercontent.com/platformps/LoanDataset/main/loan_data.json'
 response = requests.get(api_url)
 loan_data = response.json()
+
+print('API endpoint status code: ' + str(response.status_code))
